1da66fcf7df3f86234e8e97e54b7134eb8098d2c
Brenda Streiff <git@streiff.net>
2025-02-19 11:53:15 -0600

    model: item: match source types

--- src/model/item.rs
+++ src/model/item.rs
@@ -11,27 +11,29 @@ pub struct Item {
     // rarity
 
     // phys damage (DamagePhys)
-    pub damage_phys: u32,
-    pub damage_mag: u32,
-    pub delay_ms: u32,
-    pub block_rate: u32,
-    pub block: u32,
-    pub defense_phys: u32,
-    pub defense_mag: u32,
+    pub damage_phys: u16,
+    pub damage_mag: u16,
+    pub delay_ms: u16,
+    pub block_rate: u16,
+    pub block: u16,
+    pub defense_phys: u16,
+    pub defense_mag: u16,
+    pub cast_time: u8,
+    pub cooldown: u16,
     // auto-attack time
     // delay
 
     // classes
     // min lvl requirement
-    pub level_equip: u32,
+    pub level_equip: u8,
 
     // stat bonuses
-    pub materia_slot_count: u32,
+    pub materia_slot_count: u8,
     pub advanced_melds_permitted: bool,
 
     pub is_unique: bool,
     pub is_untradable: bool,
     pub can_be_hq: bool,
-    pub dye_count: u32,
+    pub dye_count: u8,
     pub is_crest_worthy: bool,
 }
